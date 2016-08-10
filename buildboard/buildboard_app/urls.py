16e863984af75636a15e2ac6970da8075b5fd3d6
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2016-08-10 13:36:01 -0400

    Added basic authentication with google view

--- buildboard/buildboard_app/urls.py
+++ buildboard/buildboard_app/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^accounts/', include('django.contrib.auth.urls', namespace='accounts')),
 ]
--- buildboard/buildboard_app/views.py
+++ buildboard/buildboard_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
+@login_required
 def index(request):
-  return HttpResponse("Welcome to Buildboard")
+  return render(request, 'index.html', {})
