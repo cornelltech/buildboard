26526b6f4cecaa1be2337793d8749ea202868af1
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2016-08-09 20:35:51 -0400

    Activated reversion integration with django admin

--- buildboard/buildboard_app/admin.py
+++ buildboard/buildboard_app/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
+from reversion.admin import VersionAdmin
 
 from .models import Semester, Company, Tag, Project, Student, Membership
 
-# Register your models here.
 
+# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(VersionAdmin):
+    pass
 
 admin.site.register(Semester)
 admin.site.register(Company)
 admin.site.register(Tag)
-admin.site.register(Project)
 admin.site.register(Student)
 admin.site.register(Membership)
