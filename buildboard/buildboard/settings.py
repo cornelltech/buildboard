51756882e5acacbaadb7d7ceb67b555627f5cbb4
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2016-09-21 00:04:36 -0400

    Removed migrations since moving db from sqlite3 to postgres. Updated project DB settings and added missing requirements.

--- buildboard/buildboard/settings.py
+++ buildboard/buildboard/settings.py
@@ -86,11 +86,13 @@ WSGI_APPLICATION = 'buildboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
