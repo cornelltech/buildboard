3b781af9ff71d5101f76435f477766ce4ead457d
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2016-09-22 00:03:00 -0400

    Added proxy model to order user by first name and last name

--- buildboard/accounts/models.py
+++ buildboard/accounts/models.py
@@ -7,11 +7,17 @@ from django.contrib.auth.models import User
 from django.conf import settings
 from django.dispatch import receiver
 
+
+class OrderedUser(User):
+    class Meta:
+        ordering = ["first_name", "last_name"]
+        proxy = True
+
 # Create your models here.
 class Account(models.Model):
     class Meta:
         ordering = ['user']
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(OrderedUser, on_delete=models.CASCADE)
     slug = models.CharField(max_length=200, blank=True)
     avatar = models.URLField(blank=True)
 
@@ -20,6 +26,7 @@ class Account(models.Model):
             first_name=self.user.first_name,
             last_name=self.user.last_name,
             email=self.user.email)
+
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_account(sender, instance=None, created=False, **kwargs):
